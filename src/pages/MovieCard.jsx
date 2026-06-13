00bb48dcf16689c0af65157767a38399491a9b7f
Adrian Santibanez <santibanez1996@gmail.com>
2026-06-12 20:44:56 -0500

    Complete movie directory routing lab

--- src/pages/MovieCard.jsx
+++ src/pages/MovieCard.jsx
@@ -1,11 +1,20 @@
+import { useOutletContext, useParams } from "react-router-dom";
 
 function MovieCard() {
-  // Replace director and movie
-  const director = null
-  if (!director) return <h2>Director not found.</h2>
-  
-  const movie = null
-  if (!movie) return <h2>Movie not found.</h2>
+  const { directors } = useOutletContext();
+  const { id, movieId } = useParams();
+
+  const director = directors.find((d) => d.id === id);
+
+  if (!director) {
+    return <h2>Director not found.</h2>;
+  }
+
+  const movie = director.movies.find((m) => m.id === movieId);
+
+  if (!movie) {
+    return <h2>Movie not found.</h2>;
+  }
 
   return (
     <div>
@@ -13,7 +22,7 @@ function MovieCard() {
       <p>⏱️ Duration: {movie.time} minutes</p>
       <p>🎬 Genres: {movie.genres.join(", ")}</p>
     </div>
-  )
+  );
 }
 
-export default MovieCard
+export default MovieCard;
